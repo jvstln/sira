993911b5f70b44c25cb6f80fdc98eb965e0a5953
abuchirobert <abuchiahamefula2019@gmail.com>
2025-06-15 00:56:58 +0100

    Changed baseurl to live backend api url

--- src/services/axios.config.ts
+++ src/services/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://sira.sportpundit.com.ng/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
